07355286b99d7ca50ca464c31a4e416bb9f5c077
Liam Sexton <liam.sexton@puppet.com>
2021-01-19 12:55:40 +0000

    added webserver commands

--- tasks/apachewebserver.sh
+++ tasks/apachewebserver.sh
@@ -2,7 +2,14 @@
 
 if [[ $PT_action = "install" ]]
 then
-    echo loool
+    sudo yum update httpd
+    sudo yum install httpd
+    sudo yum update firewalld
+    sudo yum install firewalld
+    sudo firewall-cmd --permanent --add-service=http
+    sudo firewall-cmd --reload
+    sudo systemctl start httpd
+    echo up and running!
 elif [[ $PT_action = "uninstall" ]]
 then
     echo meme
