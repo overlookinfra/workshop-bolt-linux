c561a1d0a6bc1c9daf34d072b42d617f8835ec1e
Liam Sexton <liam.sexton@puppet.com>
2021-01-19 12:58:53 +0000

    added in commands

--- tasks/apachewebserver.sh
+++ tasks/apachewebserver.sh
@@ -3,9 +3,9 @@
 if [[ $PT_action = "install" ]]
 then
     sudo yum update httpd
-    sudo yum install httpd
+    sudo yum install httpd -y
     sudo yum update firewalld
-    sudo yum install firewalld
+    sudo yum install firewalld -y
     sudo firewall-cmd --permanent --add-service=http
     sudo firewall-cmd --reload
     sudo systemctl start httpd
