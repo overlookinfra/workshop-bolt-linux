020280e43b7e0fbc3afbf4d76720b73e7e167ac3
Liam Sexton <liam.sexton@puppet.com>
2021-02-09 20:22:56 +0000

    updated plan added new task

--- tasks/apachewelcome.sh
+++ tasks/apachewelcome.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-if [[ $PT_enable = "false" ]]
+if [[ -e '/etc/httpd/conf.d/welcome.conf/' ]]
 then
     mv /etc/httpd/conf.d/welcome.conf /etc/httpd/conf.d/welcome.conf_backup
 
-elif [[ $PT_enable = "true" ]]
-then
-    mv /etc/httpd/conf.d/welcome.conf_backup /etc/httpd/conf.d/welcome.conf
-
+else
+    echo "Welcome message already disabled!"
 fi
+
